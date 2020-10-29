33cbb35d16898249f62c41d030964489553d5cf5
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-10-29 12:19:40 -0700

    Show Bitcoin closing prices from last month on a chart.js chart - BMR

--- challenge_2/client/components/app.jsx
+++ challenge_2/client/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Chart from 'chart.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -15,22 +16,55 @@ class App extends React.Component {
   }
 
   getData() {
-    // const { searchFieldValue, currentPage, perPage } = this.state;
-    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=2020-08-28&end=2020-10-28`)
+    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=2020-09-28&end=2020-10-28`)
       .then((results) => {
         this.setState({
           data: results.data
-        }, () => {console.log(this.state)});
+        }, () => { this.makeChart() });
+        // }, () => console.log(this.state));
       })
       .catch((err) => {
         console.log(err);
       })
   }
 
+  makeChart() {
+    var ctx = document.getElementById('myChart');
+    var values = Object.values(this.state.data.bpi);
+    var days = Object.keys(this.state.data.bpi);
+    console.log('values', values);
+    console.log('days', days);
+    var chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: days,
+        datasets: [
+          {
+            data: values,
+            borderColor: "#3e95cd",
+            fill: false,
+            label: "Price data for BITCOIN between Sept 28 and Oct 28"
+          }
+        ]
+      },
+      options: {
+        responsive: false,
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+              unit: 'day'
+            }
+          }]
+        }
+      }
+    });
+  }
+
   render() {
     return (
       <div>
-        hii
+        <canvas id="myChart" width="600" height="400"></canvas>
       </div>)
   }
 }
