9ea7a569507b0991b2b30c6698ef0b051dde7d3d
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-10-29 01:24:42 -0700

    Get data from Coindesk

--- challenge_2/client/components/app.jsx
+++ challenge_2/client/components/app.jsx
@@ -1,40 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     searchFieldValue: '',
-  //     resultList: [],
-  //     perPage: 10,
-  //     offset: 0,
-  //     currentPage: 0,
-  //     total: 0,
-  //     pageCount: 0
-  //   }
-  //   this.getData = this.getData.bind(this);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+    this.getData = this.getData.bind(this);
+  }
 
+  componentDidMount() {
+    this.getData();
+  }
 
-
-  // getData() {
-  //   const { searchFieldValue, currentPage, perPage } = this.state;
-  //   axios.get(`/events?q=${searchFieldValue}&_page=${currentPage + 1}&_limit=${perPage}`)
-  //     .then((results) => {
-  //       const total = results.headers["x-total-count"];
-  //       const pageCount = total / this.state.perPage;
-  //       this.setState({
-  //         resultList: results.data,
-  //         currentPage: 0,
-  //         total,
-  //         pageCount
-  //       }, () => {console.log(this.state)});
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     })
-  // }
-
+  getData() {
+    // const { searchFieldValue, currentPage, perPage } = this.state;
+    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=2020-08-28&end=2020-10-28`)
+      .then((results) => {
+        this.setState({
+          data: results.data
+        }, () => {console.log(this.state)});
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 
   render() {
     return (
