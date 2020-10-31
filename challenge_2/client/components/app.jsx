c6d7c2d39e7555dd2030067730f0ad3b11f2e873
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-10-31 01:36:26 -0700

    Refactor + implement custom date range

--- challenge_2/client/components/app.jsx
+++ challenge_2/client/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Chart from 'chart.js';
+import { Line } from 'react-chartjs-2';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -14,122 +15,24 @@ import {
   faSearchDollar,
   faMoneyBillWave,
   faChartLine,
-  faWallet
+  faWallet,
+  faCalendarAlt
 } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fab, faEuroSign, faDollarSign, faPoundSign, faYenSign, faRubleSign, faCoins, faSearchDollar, faMoneyBillWave, faChartLine, faWallet);
+library.add(fab, faEuroSign, faDollarSign, faPoundSign, faYenSign, faRubleSign, faCoins, faSearchDollar, faMoneyBillWave, faChartLine, faWallet, faCalendarAlt);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pricesLastMonth: [],
       pricesLastYear: [],
       currentPriceUSD: 0,
       currentPriceEUR: 0,
       currentPriceGBP: 0,
       currentPriceCYN: 0,
-      currentPriceRUB: 0
-    }
-    this.getHistoricalData = this.getHistoricalData.bind(this);
-    this.getCurrentData = this.getCurrentData.bind(this);
-    this.handleRefreshClick = this.handleRefreshClick.bind(this);
-  }
-
-  componentDidMount() {
-    this.getHistoricalData();
-    this.getCurrentData();
-  }
-
-  handleRefreshClick(e) {
-    e.preventDefault();
-    this.getCurrentData();
-  }
-
-  getHistoricalData() {
-    var today = new Date();
-    console.log(today);
-
-    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
-      .then((pricesLastMonth) => {
-        this.setState({
-          pricesLastMonth: pricesLastMonth.data,
-        }, () => { this.makeChart() });
-        // }, () => console.log(this.state));
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-
-    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-10-21&end=2020-10-28')
-      .then((pricesLastYear) => {
-        this.setState({
-          pricesLastYear: pricesLastYear.data,
-        }, () => { this.makeChart2() });
-        // }, () => console.log(this.state));
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }
-
-  getCurrentData() {
-    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then((currentPrice) => {
-        console.log(currentPrice)
-        this.setState({
-          currentPriceUSD: currentPrice.data.bpi['USD'].rate.split('.')[0],
-          currentPriceEUR: currentPrice.data.bpi['EUR'].rate.split('.')[0],
-          currentPriceGBP: currentPrice.data.bpi['GBP'].rate.split('.')[0]
-        }, () => console.log(this.state));
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-
-    axios.get('https://api.coindesk.com/v1/bpi/currentprice/CNY.json')
-      .then((currentPrice) => {
-        console.log(currentPrice)
-        this.setState({
-          currentPriceCYN: currentPrice.data.bpi['CNY'].rate.split('.')[0]
-        }, () => console.log(this.state));
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-
-    axios.get('https://api.coindesk.com/v1/bpi/currentprice/RUB.json')
-      .then((currentPrice) => {
-        console.log(currentPrice)
-        this.setState({
-          currentPriceRUB: currentPrice.data.bpi['RUB'].rate.split('.')[0]
-        }, () => console.log(this.state));
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }
-
-  // setTimeout(getCurrentData, 60000)
-
-  makeChart() {
-    var ctx = document.getElementById('myChart');
-    var values = Object.values(this.state.pricesLastMonth.bpi);
-    var days = Object.keys(this.state.pricesLastMonth.bpi);
-    var chart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        borderColor: "#000",
-        labels: days,
-        datasets: [
-          {
-            data: values,
-            borderColor: "#282828",
-            fill: false
-          },
-        ]
-      },
-      options: {
+      currentPriceRUB: 0,
+      chart1Data: {},
+      chart1Options: {
         title: {
           display: true,
           fontSize: 16,
@@ -155,8 +58,6 @@ class App extends React.Component {
             }
           }
         },
-        // responsive: false,
-        // maintainAspectRatio: true,
         scales: {
           xAxes: [{
             type: 'time',
@@ -176,33 +77,14 @@ class App extends React.Component {
             }
           }]
         }
-      }
-    });
-  }
-
-  makeChart2() {
-    var ctx = document.getElementById('myChart2');
-    var values = Object.values(this.state.pricesLastYear.bpi);
-    var months = Object.keys(this.state.pricesLastYear.bpi);
-    var chart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        borderColor: "#000",
-        labels: months,
-        datasets: [
-          {
-            data: values,
-            borderColor: "#282828",
-            fill: false
-          },
-        ]
       },
-      options: {
+      chart2Data: {},
+      chart2Options: {
         title: {
           display: true,
           fontSize: 16,
           fontFamily: "'Jura', sans-serif",
-          text: 'BITCOIN price changes in the last year',
+          text: 'BITCOIN price changes',
           padding: 20
         },
         layout: {
@@ -223,8 +105,6 @@ class App extends React.Component {
             }
           }
         },
-        // responsive: false,
-        // maintainAspectRatio: true,
         scales: {
           xAxes: [{
             type: 'time',
@@ -244,8 +124,104 @@ class App extends React.Component {
             }
           }]
         }
-      }
+      },
+      startDate: '2017-01-01',
+      endDate: new Date().toISOString().substr(0, 10),
+    }
+    this.getHistoricalData = this.getHistoricalData.bind(this);
+    this.getCurrentData = this.getCurrentData.bind(this);
+    this.handleRefreshClick = this.handleRefreshClick.bind(this);
+    this.handleStartDateChange = this.handleStartDateChange.bind(this);
+    this.handleEndDateChange = this.handleEndDateChange.bind(this);
+    this.fetchDateRange = this.fetchDateRange.bind(this);
+  }
+
+  componentDidMount() {
+    this.getHistoricalData();
+    this.getCurrentData();
+  }
+
+  handleRefreshClick(e) {
+    e.preventDefault();
+    this.getCurrentData();
+  }
+
+  handleStartDateChange(e) {
+    const date = e.target.value;
+    this.fetchDateRange(date, this.state.endDate);
+    this.setState({
+      startDate: date,
     });
+  }
+
+  handleEndDateChange(e) {
+    const date = e.target.value;
+    this.fetchDateRange(this.state.startDate, date);
+    this.setState({
+      endDate: date,
+    });
+  }
+
+  getChartDataFromCoindeskResponse(response) {
+    var values = Object.values(response.data.bpi);
+    var keys = Object.keys(response.data.bpi);
+    return {
+      borderColor: "#000",
+      labels: keys,
+      datasets: [
+        {
+          data: values,
+          borderColor: "#282828",
+          fill: false
+        },
+      ],
+    };
+  }
+
+  fetchDateRange(startDate, endDate) {
+    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
+      .then((response) => {
+        this.setState({
+          chart2Data: this.getChartDataFromCoindeskResponse(response),
+        });
+      });
+  }
+
+  getHistoricalData() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+      .then((response) => {
+        const data = this.getChartDataFromCoindeskResponse(response);
+        this.setState({
+          chart1Data: data,
+        });
+      });
+
+    this.fetchDateRange(this.state.startDate, this.state.endDate);
+  }
+
+  getCurrentData() {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then((response) => {
+        this.setState({
+          currentPriceUSD: response.data.bpi['USD'].rate.split('.')[0],
+          currentPriceEUR: response.data.bpi['EUR'].rate.split('.')[0],
+          currentPriceGBP: response.data.bpi['GBP'].rate.split('.')[0]
+        });
+      });
+
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice/CNY.json')
+      .then((response) => {
+        this.setState({
+          currentPriceCYN: response.data.bpi['CNY'].rate.split('.')[0]
+        });
+      });
+
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice/RUB.json')
+      .then((response) => {
+        this.setState({
+          currentPriceRUB: response.data.bpi['RUB'].rate.split('.')[0]
+        });
+      });
   }
 
   render() {
@@ -276,7 +252,11 @@ class App extends React.Component {
             <div className="left-col">
               <h3><FontAwesomeIcon icon="chart-line" className="search" /><span>Recent changes</span></h3>
               <p>Bitcoin can be bought on exchanges or directly from other people via marketplaces.<br />You can purchase bitcoin in a variety of ways, using anything from hard cash to credit and debit cards to wire transfers, or even other cryptocurrencies, depending on who you are buying them from and where you live.<br /><br />Contrary to popular belief, the crypto market is the most difficult to trade for beginners. Before you start, check out the the recent changes and make sure you are up-to-date!</p>
-              <canvas id="myChart"></canvas>
+
+              <div className="chart month">
+                <Line data={this.state.chart1Data} options={this.state.chart1Options} />
+              </div>
+
               <span className="cd">Powered by coindesk</span>
             </div>
             <div className="right-col">
@@ -287,12 +267,22 @@ class App extends React.Component {
           <section className="canvas2">
             <div className="left-col">
               <h3><FontAwesomeIcon icon="wallet" className="search" /><span>Bitcoin Wallet</span></h3>
-              <p>Bitcoins are stored in a "digital wallet", which exists either in the cloud or on a user's computer. The wallet is a kind of virtual bank account that allows users to send or receive bitcoins, pay for goos or save their money. Unline bank accounts, bitcoin wallets are not insure by the FDIC.<br /><br />Though each bitcoin transaction is recorded in a public log, names of buyers and sellers are never revealed – only their wallet IDs. While that keeps bitcoin users’ transactions private, it also lets them buy or sell anything without easily tracing it back to them. <a href="https://money.cnn.com/infographic/technology/what-is-bitcoin/index.html">More info..</a></p>
+              <p>Bitcoins are stored in a "digital wallet", which exists either in the cloud or on a user's computer. The wallet is a kind of virtual bank account that allows users to send or receive bitcoins, pay for goods or save their money. Bitcoin wallets are not insured by the FDIC.<br /><br />Though each bitcoin transaction is recorded in a public log, names of buyers and sellers are never revealed – only their wallet IDs. While that keeps bitcoin users’ transactions private, it also lets them buy or sell anything without easily tracing it back to them. <br/><a href="https://money.cnn.com/infographic/technology/what-is-bitcoin/index.html">More info..</a></p>
             </div>
             <div className="right-col">
-              <h3><FontAwesomeIcon icon="chart-line" className="search" /><span>Changes last year</span></h3>
-              <p>Choose the dates</p>
-              <canvas id="myChart2"></canvas>
+              <h3><FontAwesomeIcon icon="calendar-alt" className="search" /><span>Custom date range</span></h3>
+
+              <label>Start date
+                <input type="date" value={this.state.startDate} onChange={this.handleStartDateChange}></input>
+              </label>
+              <label>End date
+                <input type="date" value={this.state.endDate} onChange={this.handleEndDateChange}></input>
+              </label>
+
+              <div className="chart custom">
+                <Line data={this.state.chart2Data} options={this.state.chart2Options} />
+              </div>
+
               <span className="cd">Powered by coindesk</span>
             </div>
           </section>
