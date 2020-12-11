fc3aa7bf786a694e30567b16e75f560c2ff26f34
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-12-10 19:12:31 -0800

    Small fix

--- challenge_4/minesweeper/src/store/store.js
+++ challenge_4/minesweeper/src/store/store.js
@@ -1,12 +1,9 @@
 import { createStore } from 'redux';
-// import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
 import rootReducer from '../reducers/main.js';
 
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  // applyMiddleware(thunk)
 );
 
 export default store;
